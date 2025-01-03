e95f69616f75e19c8624fbf7e0ed2ff1eacc4df6
Uciel Sola <solauciel@gmail.com>
2025-01-03 13:55:38 -0300

    comment out Homebrew installation command in setup script

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@
 
 ## Brew
 ### Install brew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 ### Enable brew
 # (
