0b163bf0ddd7676a56f66634f19edceabda7bc99
Uciel Sola <solauciel@gmail.com>
2025-10-02 14:11:57 -0300

    add nvim to dotfiles

---
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,17 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    window = {
+      position = "right",
+    },
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+        hide_gitignored = false,
+        hide_by_name = {},
+        never_show = {},
+      },
+    },
+  },
+}
